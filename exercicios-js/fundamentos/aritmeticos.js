4fb76a987dd7a7a2696e9e5851994c486d949565
felipe-novais <felipesn17@sempreceub.com>
2019-12-28 09:22:36 -0300

    Fundamentos Finalizados

---
+++ exercicios-js/fundamentos/aritmeticos.js
@@ -0,0 +1,9 @@
+const [a,b,c,d] = [3,5,1,15]
+
+const soma = a + b + c + d
+const subtração = d - b
+const multiplicação = a * b
+const divisão = d / a
+const modulo = a % 2
+
+console.log(-soma, subtração, -multiplicação, divisão, -modulo)
