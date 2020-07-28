8a8053f27f21407ea41d8c42d68145c1d46bd396
Felipe <felipekun213@gmail.com>
2020-07-28 17:59:32 -0300

    Conceitos de Ajax

--- exercicios-web/dom/js/test.js
+++ exercicios-web/dom/js/test.js
@@ -1,43 +1,23 @@
-function novoElemento(tagName, className) {
-    const elem = document.createElement(tagName)
-    elem.className = className
-    return elem
+function estaoSobrepostos(elementoA, elementoB) {
+    const a = elementoA.getBoundingClientRect()
+    const b = elementoB.getBoundingClientRect()
+
+    const horizontal = a.left + a.width >= b.left
+        && b.left + b.width >= a.left
+    const vertical = a.top + a.height >= b.top
+        && b.top + b.height >= a.top
+    return horizontal && vertical
 }
 
-function Barreira(reversa = false) {
-    this.elemento = novoElemento('div', 'barreira')
-
-    const borda = novoElemento('div', 'borda')
-    const cano = novoElemento('div', 'cano')
-    this.elemento.appendChild(reversa ? cano : borda)
-    this.elemento.appendChild(reversa ? borda : cano)
-
-    this.setAltura = altura => cano.style.height = `${altura}px`
+function colidiu(passaro, barreiras) {
+    let colidiu = false
+    barreiras.pares.forEach(parDeBarreiras => {
+        if (!colidiu) {
+            const superior = parDeBarreiras.superior.elemento
+            const inferior = parDeBarreiras.inferior.elemento
+            colidiu = estaoSobrepostos(passaro.elemento, superior)
+                || estaoSobrepostos(passaro.elemento, inferior)
+        }
+    })
+    return colidiu
 }
-
-function ParDeBarreiras(altura, abertura, x) {
-    this.elemento = novoElemento('div', 'barreiras')
-
-    this.superior = new Barreira(true)
-    this.inferior = new Barreira(false)
-
-    this.elemento.appendChild(this.superior.elemento)
-    this.elemento.appendChild(this.inferior.elemento)
-
-    this.sortearAbertura = () => {
-        const alturaSuperior = Math.random() * (altura - abertura)
-        const alturaInferior = altura - abertura - alturaSuperior
-        this.superior.setAltura(alturaSuperior)
-        this.inferior.setAltura(alturaInferior)
-    }
-
-    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
-    this.setX = x => this.elemento.style.left = `${x}px`
-    this.getLargura = () => this.elemento.clientWidth
-
-    this.sortearAbertura()
-    this.setX(x)
-}
-
-const b = new ParDeBarreiras(700, 200, 800)
-document.querySelector('[flappy]').appendChild(b.elemento)
